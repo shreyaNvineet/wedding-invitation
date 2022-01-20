c817e8d80e7c2d4db7d691a20e1160af3220766b
ShreyamChandra <95626322+ShreyamChandra@users.noreply.github.com>
2022-01-20 14:48:22 +0530

    nab-bar-feature

--- js/resp.js
+++ js/resp.js
@@ -1,8 +1,44 @@
 burger =document.querySelector('.burger')
 navList = document.querySelector('.nav-list')
 navBar = document.querySelector('.navbar')
+navItem = document.querySelector('.nav-item');
+navItem2 = document.querySelector('.nav-item2');
+navItem3 = document.querySelector('.nav-item3');
+navItem4 = document.querySelector('.nav-item4');
+navItem5 = document.querySelector('.nav-item5');
+navItem6 = document.querySelector('.nav-item6');
 
 burger.addEventListener('click',()=>{
     navList.classList.toggle('v-class'); 
+    navBar.classList.toggle('h-nav')
+})
+
+navItem.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class');
+    navBar.classList.toggle('h-nav')
+})
+
+navItem2.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class');
+    navBar.classList.toggle('h-nav')
+})
+
+navItem3.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class');
+    navBar.classList.toggle('h-nav')
+})
+
+navItem4.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class');
+    navBar.classList.toggle('h-nav')
+})
+
+navItem5.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class');
+    navBar.classList.toggle('h-nav')
+})
+
+navItem6.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class');
     navBar.classList.toggle('h-nav')
 })
